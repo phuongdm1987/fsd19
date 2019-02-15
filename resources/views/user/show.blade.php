edc87f78e1e081a3a7b0eede7eadf5fea9eb8609
Henry <phuongdm1987@gmail.com>
2019-02-15 22:42:47 +0700

    remove jquery upload

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -12,7 +12,7 @@
 @stop
 @section('scripts')
 	<script src="{{ asset('js/jquery.ui.widget.js') }}"></script>
-  	<script src="{{ asset('js/jquery.fileupload.js') }}"></script>
+{{--  	<script src="{{ asset('js/jquery.fileupload.js') }}"></script>--}}
   	<script src="{{ asset('js/jquery.bootstrap-growl.min.js') }}"  type="text/javascript"></script>
 	<script src="{{ asset('js/user-wall.js') }}" type="text/javascript"></script>
 	<script src="{{ asset('js/follower.js') }}" type="text/javascript"></script>
